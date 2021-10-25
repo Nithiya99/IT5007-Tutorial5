1cd5fc110d27c9f35816f402da69f3c59134a838
Nithiya <nithiya070399@gmail.com>
2021-10-25 16:29:34 +0800

    client initialized

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import WaitList from "./components/WaitList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hotel California Waiting List Portal</h1>
+      <WaitList />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/WaitList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class WaitList extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="wait-list">
+          <li>Customer Name</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default WaitList;
